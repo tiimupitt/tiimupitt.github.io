fae21a253baf8b72f71320f61f1ff20a3ffff253
tiimuAdaptive <81987736+tiimupitt@users.noreply.github.com>
2026-03-14 14:52:29 +0000

    add strict linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,28 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import css from '@eslint/css'
+import stylistic from '@stylistic/eslint-plugin'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettier from 'eslint-config-prettier/flat'
+import perfectionist from 'eslint-plugin-perfectionist'
+import sonarjs from 'eslint-plugin-sonarjs'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettier,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
-]);
+  perfectionist.configs['recommended-natural'],
+  stylistic.configs.recommended,
+  sonarjs.configs.recommended,
+  css.configs.recommended,
+])
 
-export default eslintConfig;
+export default eslintConfig
